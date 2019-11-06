3f93570618e33b61f24423be9b17d4f2afad5ef0
Vinicius Barenho Pereira <viniciusbarenho@gmail.com>
2019-11-06 12:30:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sorting Visualizer - Testing Sorting Algorithms
+
+Check it out at: https://vinibpereira.github.io/sorting-visualizer/build/
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import './app.css';
+function App() {
+  return <SortingVisualizer />;
+}
+
+export default App;
